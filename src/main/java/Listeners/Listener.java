84b77ff771b0627e0e31da1efd944b76080f09bd
kritika <kritika9648@gmail.com>
2024-06-12 00:05:45 +0530

    Second commit

--- src/main/java/Listeners/Listener.java
+++ src/main/java/Listeners/Listener.java
@@ -6,42 +6,42 @@ import org.testng.ITestResult;
 
 public class Listener implements ITestListener{
 
-	@Override
+	
 	public void onTestStart(ITestResult result) {
 	
 	}
 
-	@Override
+	
 	public void onTestSuccess(ITestResult result) {
 	
 	}
 
-	@Override
+	
 	public void onTestFailure(ITestResult result) {
 	
 	}
 
-	@Override
+	
 	public void onTestSkipped(ITestResult result) {
 		
 	}
 
-	@Override
+	
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 	
 	}
 
-	@Override
+	
 	public void onTestFailedWithTimeout(ITestResult result) {
 		
 	}
 
-	@Override
+	
 	public void onStart(ITestContext context) {
 	
 	}
 
-	@Override
+	
 	public void onFinish(ITestContext context) {
 	
 	}
